6da0f5bc506ba1d112a076e8d1f9866157278d6c
MAILSON\mails <mailsoncouto4@gmail.com>
2024-01-03 17:30:00 -0300

    Adicionado

---
+++ fundamentos/sintaxe.js
@@ -0,0 +1,12 @@
+/**
+ * Tutorial Node.js
+ * Sintaxe Básica 
+ */
+
+//Sentenças
+console.log('Sintaxe básica')
+
+//Blocos
+{
+    console.log('Blocos')
+}
---
+++ fundamentos/variaveis.js
@@ -0,0 +1,5 @@
+/**
+ * Tutorial Node.js
+ * Variáveis
+ */
+
